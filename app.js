621c37cc0c081be17b8cc4d7dd5bf11ed77f6d3a
shijinyu <shijinyu@163.com>
2018-02-02 16:03:15 +0800

    test

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          //发起网络请求  
+          console.log(res.code)
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        } 
       }
     })
     // 获取用户信息
--- pages/index/index.js
+++ pages/index/index.js
@@ -56,5 +56,25 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  getPhoneNumber: function (e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+    if (e.detail.errMsg == 'getPhoneNumber:fail user deny') {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '未授权',
+        success: function (res) { }
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        showCancel: false,
+        content: '同意授权',
+        success: function (res) { }
+      })
+    }
   }
 })
